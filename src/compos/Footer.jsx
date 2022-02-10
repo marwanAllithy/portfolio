2d2a2b89f2c2111fcd75b4d027b872e7e06de602
OZY <92400495+OZY3112@users.noreply.github.com>
2022-02-10 21:12:03 +0200

    init

--- src/compos/Footer.jsx
+++ src/compos/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
                     <a href='#navbar' className="footer__logo click"><img src={Logo} alt="" className="footer__logo--img" /></a>
                 </div>
                 <ul className="footer__link--list">
-                    <li className="footer__link hover-effect hover-effect--dark"><a href="#aboutme" className="footer__link--anchor">About me</a></li>
+                    <li className="footer__link hover-effect hover-effect--dark"><a href="#aboutme" className="footer__link--anchor">About Me</a></li>
                     <li className="footer__link hover-effect hover-effect--dark"><a href="#skills" className="footer__link--anchor">Skills</a></li>
-                    <li className="footer__link hover-effect hover-effect--dark"><a href="#projects" className="footer__link--anchor">Portjects</a></li>
-                    <li className="footer__link hover-effect hover-effect--dark"><a href="#contact" className="footer__link--anchor">Contact</a></li>
+                    <li className="footer__link hover-effect hover-effect--dark"><a href="#projects" className="footer__link--anchor">Porjects</a></li>
+                    <li className="footer__link hover-effect hover-effect--dark"><a href="#contact" className="footer__link--anchor">Contact Me</a></li>
                 </ul>
                 <div className="footer__copy-right">Copyright &copy; 2022 Marwan Allisy</div>
             </div>
--- src/compos/Nav.jsx
+++ src/compos/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = ({handledarkmode, handleLightmode, darkmode, lightmode}) => {
         <nav id="navbar">
             <figure className="nav__logo"><img src={lightmode ? Logolightmde : Logodarkmode} alt="" className="nav__logo" /></figure>
             <ul className="nav__link--list" >
-                <li className={`nav__link hover-effect hover-effect--${lightmode ?"white" : "dark" }`}  ><a style={{color: lightmode ? "black" : "#e0e0e0"}} href="#aboutme" className="nav__link--anchor">About me</a></li>
+                <li className={`nav__link hover-effect hover-effect--${lightmode ?"white" : "dark" }`}  ><a style={{color: lightmode ? "black" : "#e0e0e0"}} href="#aboutme" className="nav__link--anchor">About Me</a></li>
                 <li className={`nav__link hover-effect hover-effect--${lightmode ?"white" : "dark" }`}><a style={{color: lightmode ? "black" : "#e0e0e0"}} href="#skills" className="nav__link--anchor">Skills</a></li>
                 <li className={`nav__link hover-effect hover-effect--${lightmode ?"white" : "dark" }`}><a style={{color: lightmode ? "black" : "#e0e0e0"}} href="#projects" className="nav__link--anchor">Projects</a></li>
                 <li className={`nav__link hover-effect hover-effect--${lightmode ?"white" : "dark" } nav__link-contact`}><a style={{color: lightmode ? "black" : "#e0e0e0"}} href="#contact" className="nav__link--anchor click">Contact Me</a></li>
