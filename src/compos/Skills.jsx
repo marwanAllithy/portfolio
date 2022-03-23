e306cd7448454a3e8c22391411dae48c66515fc5
OZY <92400495+OZY3112@users.noreply.github.com>
2022-03-23 17:43:16 +0200

    init

--- src/compos/Skills.jsx
+++ src/compos/Skills.jsx
@@ -1,19 +1,5 @@
-import React from 'react';
-import react from './techimgs/React-icon.png'
-import JS from './techimgs/js-icon.png'
-import firebase from './techimgs/firebase-icon.png'
-import html from './techimgs/html-icon.svg'
-import Node from './techimgs/node-logo.png'
-import sass from './techimgs/SASS-icon.png'
-import ts from './techimgs/typescript-icon.svg'
 import Techitem from './ui/techitem';
-import css from './techimgs/css.svg'
-import bootstrap from './techimgs/Bootstrap_logo.svg.png'
-import vite from './techimgs/vitelogo.svg'
-import Mantine from './techimgs/mantine.png'
-import tailwind from './techimgs/tailwind.png'
-import Nextjs from './techimgs/Nextjs.png'
-import MaterialUI from './techimgs/MaterialUILOGO.png'
+import { ProficientArray, familiarArraw } from './helper/skillArray'
 import AOS  from 'aos';
 import "aos/dist/aos.css";
 AOS.init();
@@ -37,18 +23,11 @@ const Skills = ({lightmode, darkmode}) => {
                         </div>
                         <div className="skills__cont"data-aos="fade-up" >
                         <div className="skills">
-                            <Techitem fontColor='#eb5c28' lightmode={lightmode} data-aos="fade-up"data-aos-duration="400" techname="HTML" techimg={html}/>
-                            <Techitem fontColor='#f7e018' lightmode={lightmode}  data-aos="fade-up"data-aos-duration="200" techname="JavaScript" techimg={JS}/>
-                            <Techitem fontColor='#1b73ba' lightmode={lightmode} data-aos="fade-up"data-aos-duration="700" techname="CSS" techimg={css}/>
-                            <Techitem fontColor='#61dbfb' lightmode={lightmode} data-aos="fade-up"data-aos-duration="500" techname="React.js" techimg={react} />
-                            <Techitem fontColor='#75ac63' lightmode={lightmode} data-aos="fade-up"data-aos-duration="500" techname="Node.js" techimg={Node}/>
-                            <Techitem fontColor='#000000' lightmode={lightmode} data-aos="fade-up"data-aos-duration="500" techname="Next.js" techimg={Nextjs} />
-                            <Techitem fontColor='#d1689c' lightmode={lightmode} data-aos="fade-up"data-aos-duration="100" techname="SASS" techimg={sass}/>
-                            <Techitem fontColor='#fcca3f' lightmode={lightmode}  data-aos="fade-up"data-aos-duration="500" techname="Firebase" techimg={firebase}/>
-                            <Techitem fontColor='#13c3b6' lightmode={lightmode} data-aos="fade-up"data-aos-duration="500" techname="Tailwind" techimg={tailwind}/> 
-                            <Techitem fontColor='#ffd32b' lightmode={lightmode}  data-aos="fade-up"data-aos-duration="500" techname="Vite" techimg={vite}/>
-                            <Techitem fontColor='#339af0' lightmode={lightmode}  data-aos="fade-up"data-aos-duration="500" techname="Mantine" techimg={Mantine}/>
-                            
+                            {
+                                ProficientArray.map(item => (
+                                    <Techitem fontColor={item.fontColor} lightmode={lightmode} data-aos="fade-up"data-aos-duration="400" techname={item.name} techimg={item.img}/>
+                                ))
+                            }
                         </div>
                     </div>
                     </div>
@@ -62,9 +41,11 @@ const Skills = ({lightmode, darkmode}) => {
                     </div>
                     <div className="skills__cont" data-aos="fade-up">
                         <div className="skills">
-                            <Techitem  fontColor='#339af0' lightmode={lightmode} data-aos="fade-up"data-aos-duration="500" techname="TypeScript" techimg={ts}/> 
-                            <Techitem  fontColor='#007fff' lightmode={lightmode} data-aos="fade-up"data-aos-duration="500" techname="MUI" techimg={MaterialUI}/> 
-                            <Techitem  fontColor='#7708f6' lightmode={lightmode} data-aos="fade-up"data-aos-duration="500" techname="BootStrap" techimg={bootstrap}/> 
+                            {
+                                familiarArraw.map(item => (
+                                <Techitem  fontColor={item.fontColor} lightmode={lightmode} data-aos="fade-up"data-aos-duration="500" techname={item.name} techimg={item.img}/> 
+                                ))
+                            }
                         </div>
                     </div>
                     </div>
---
+++ src/compos/helper/skillArray.js
@@ -0,0 +1,85 @@
+import react from '../techimgs/React-icon.png'
+import JS from '../techimgs/js-icon.png'
+import firebase from '../techimgs/firebase-icon.png'
+import html from '../techimgs/html-icon.svg'
+import Node from '../techimgs/node-logo.png'
+import sass from '../techimgs/SASS-icon.png'
+import ts from '../techimgs/typescript-icon.svg'
+import css from '../techimgs/css.svg'
+import vite from '../techimgs/vitelogo.svg'
+import Mantine from '../techimgs/mantine.png'
+import tailwind from '../techimgs/tailwind.png'
+import Nextjs from '../techimgs/Nextjs.png'
+import MaterialUI from '../techimgs/MaterialUILOGO.png'
+
+
+export const ProficientArray = [
+    {
+        img: html,
+        name: "HTML",
+        fontColor: "#eb5c28"
+    },
+    {
+        img: JS,
+        name: "JavaScript",
+        fontColor: "#f7e018"
+    },
+    {
+        img: css,
+        name: "CSS",
+        fontColor: "#1b73ba"
+    },
+    {
+        img: react,
+        name: "React.js",
+        fontColor: "#61dbfb"
+    },
+    {
+        img: Node,
+        name: "Node.js",
+        fontColor: "#75ac63"
+    },
+    {
+        img: Nextjs,
+        name: "Next.js",
+        fontColor: "#000000"
+    },
+    {
+        img: sass,
+        name: "SASS",
+        fontColor: "#d1689c"
+    },
+    {
+        img: firebase,
+        name: "Firebase",
+        fontColor: "#fcca3f"
+    },
+    {
+        img: tailwind,
+        name: "Tailwind",
+        fontColor: "#13c3b6"
+    },
+    {
+        img: vite,
+        name: "Vite",
+        fontColor: "#ffd32b"
+    },
+    {
+        img: Mantine,
+        name: "Mantine",
+        fontColor: "#339af0"
+    },
+]
+
+export const familiarArraw = [
+    {
+        img: ts,
+        name: "TypeScript",
+        fontColor: "#339af0"
+    },
+    {
+        img: MaterialUI,
+        name: "MUI",
+        fontColor: "#007fff"
+    },
+]
