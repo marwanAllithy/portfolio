87837c464d7b31b9148709fa4d10f073c467054b
OZY <92400495+OZY3112@users.noreply.github.com>
2022-03-23 10:06:20 +0200

    init

--- src/compos/ui/techitem.jsx
+++ src/compos/ui/techitem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Techitem = ({techname, techimg, svg, lightmode, fontColor}) => {
     return (
         <div className="tech__item--cont">
-            <div className="tech__item"  style={{boxShadow: lightmode ?  "-5px 5px 24px #000000"  :"-5px 5px 24px #000000" }}>
+            <div className="tech__item"  style={{boxShadow: lightmode ?  "-2px 2px 15px #000000"  :"-5px 5px 15px #000000" }}>
                 <div className="tech__item--img--cont">
                     <img src={techimg} alt="" className="tech__item--img" />
                 </div>
