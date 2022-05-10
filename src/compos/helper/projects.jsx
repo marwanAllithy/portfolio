1eee03b216418b4b4859cf8999dc9adc474aa27b
OZY <92400495+OZY3112@users.noreply.github.com>
2022-05-10 17:58:53 +0200

    adding wordle as a project

--- src/compos/helper/projects.jsx
+++ src/compos/helper/projects.jsx
@@ -1,21 +1,30 @@
-import librarypic from '../projectimg/library.png'
-import easyViewPic from '../projectimg/easyView.png'
+import librarypic from "../projectimg/library.png";
+import easyViewPic from "../projectimg/easyView.png";
+import wordlePic from "../projectimg/wordle.png";
 
 export const projects = [
-    {
-        img: librarypic,
-        title: "E-Commerce-library",
-        para: "Using best practices with Html (BEM), Css, and javaScript, while using React (+hooks) i built a fake E-library and a clean UI design with a easy-to-understand layout for the website.",
-        githublink: "https://github.com/OZY3112/E-Commerce-library",
-        websitelink: "https://e-commerce-library.vercel.app/",
-        tools: " CSS | JavaScript | React | Node.Js"
-    },
-    {
-        img: easyViewPic,
-        title:"Easy view",
-        para:"Using the TMDB API and Axios I made a website (in 3 days) where you can look for a movie name and find all of the results and also get access to more information by clicking on the movie poster.",
-        githublink: "https://github.com/OZY3112/views_bata",
-        websitelink: "https://easy-view.vercel.app/",
-        tools: " SCSS | JavaScript | React | Vite | axios | Node.Js"
-    }
-]
+  {
+    img: wordlePic,
+    title: "Wordle clone",
+    para: "A wordle clone built with React, using cookies and local storage to store the user's progress. ",
+    githublink: "https://github.com/OZY3112/wordle",
+    websitelink: "https://wordle-ozy.vercel.app/",
+    tools: " SCSS | Cookies | JavaScript | React | Node.Js",
+  },
+  {
+    img: librarypic,
+    title: "E-Commerce library",
+    para: "Using best practices with Html (BEM), Css, and javaScript, while using React (+hooks) i built a fake E-library and a clean UI design with a easy-to-understand layout for the website.",
+    githublink: "https://github.com/OZY3112/E-Commerce-library",
+    websitelink: "https://e-commerce-library.vercel.app/",
+    tools: " CSS | JavaScript | React | Node.Js",
+  },
+  {
+    img: easyViewPic,
+    title: "Easy view",
+    para: "Using the TMDB API and Axios I made a website (in 3 days) where you can look for a movie name and find all of the results and also get access to more information by clicking on the movie poster.",
+    githublink: "https://github.com/OZY3112/views_bata",
+    websitelink: "https://easy-view.vercel.app/",
+    tools: " SCSS | JavaScript | React | Vite | axios | Node.Js",
+  },
+];
