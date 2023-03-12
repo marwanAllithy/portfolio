63eb62b9296cbe9e17ba0a9e9f32752b55a79b95
OZY311 <mrwan311@gmail.com>
2023-03-12 16:06:18 +0200

    vercel add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()]
+  integrations: [tailwind(), react(), image()],
+  adapter: vercel(),
 });
