bbab4df7dab6dd857c5d41c66902774112810572
OZY311 <mrwan311@gmail.com>
2023-03-16 12:38:33 +0200

    fixed phone problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), image()],
-  adapter: vercel(),
 });
